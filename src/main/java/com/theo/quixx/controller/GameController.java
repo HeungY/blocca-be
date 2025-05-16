ca80d6030ee9e8c24924e463e6de472861f8bf4f
HeungY <rkrkrkrk02@naver.com>
2025-05-16 16:43:59 +0900

    feat: create HealthCheck api

--- src/main/java/com/theo/quixx/controller/GameController.java
+++ src/main/java/com/theo/quixx/controller/GameController.java
@@ -1,4 +1,15 @@
 package com.theo.quixx.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class GameController {
+
+    @GetMapping("/api/hc")
+    public ResponseEntity<?> healthcheck() {
+        return ResponseEntity.ok("Server is alive !");
+    }
+
 }

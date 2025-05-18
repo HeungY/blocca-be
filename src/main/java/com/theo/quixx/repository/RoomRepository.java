d8ca8aba72fdb25297d6716fc08f8f5dcb085e41
HeungY <rkrkrkrk02@naver.com>
2025-05-18 23:38:14 +0900

    feat: RoomRepository findByCode 생성
    Code를 통해 DB row 탐색

--- src/main/java/com/theo/quixx/repository/RoomRepository.java
+++ src/main/java/com/theo/quixx/repository/RoomRepository.java
@@ -4,4 +4,5 @@ import com.theo.quixx.domain.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
+    Room findByCode(String code);
 }

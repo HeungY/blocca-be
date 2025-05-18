6cd3f9779e4bd2474fb8101f02322318f55fe4f1
HeungY <rkrkrkrk02@naver.com>
2025-05-18 13:27:53 +0900

    chore: update room entity

--- src/main/java/com/theo/quixx/domain/Room.java
+++ src/main/java/com/theo/quixx/domain/Room.java
@@ -8,25 +8,30 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @Entity
 @Table(name = "room")
-public final class Room {
+public class Room {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Getter
     @Column(name = "firstPlayer")
     private String firstPlayer;
 
+    @Getter
     @Column(name = "secondPlayer")
     private String secondPlayer;
 
+    @Getter
     @Column(name = "isActive")
     private boolean active;
 
+    @Getter
     @Column(name = "code")
     private String code;
 
@@ -36,30 +41,17 @@ public final class Room {
     @Builder
     public Room(String firstPlayer, String code) {
         this.firstPlayer = firstPlayer;
-        this.secondPlayer = "undefined";
         this.active = true;
         this.code = code;
         this.createdAt = LocalDateTime.now();
     }
 
-    public void changeSecondPlayer(String secondPlayer) {
+    public void join(String secondPlayer) {
         this.secondPlayer = secondPlayer;
     }
 
-    public String nameFirstPlayer(String firstPlayer) {
-        return firstPlayer;
-    }
-
-    public String nameSecondPlayer(String secondPlayer) {
-        return secondPlayer;
-    }
-
-    public boolean roomIsActive() {
-        return active;
-    }
-
-    public String roomCode() {
-        return code;
+    public boolean hasSecondPlayer() {
+        return secondPlayer != null;
     }
 
     public void roomClose() {

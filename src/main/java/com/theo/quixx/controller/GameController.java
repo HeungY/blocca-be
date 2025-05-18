2c3ff31e5e226d121b38dbb28d6015e47d828d90
HeungY <rkrkrkrk02@naver.com>
2025-05-18 23:39:06 +0900

    feat: GameController 방 생성 api 작성
    /api/room/create 통해 방 생성

--- src/main/java/com/theo/quixx/controller/GameController.java
+++ src/main/java/com/theo/quixx/controller/GameController.java
@@ -1,15 +1,30 @@
 package com.theo.quixx.controller;
 
+import com.theo.quixx.dto.room.CreateRequestDTO;
+import com.theo.quixx.dto.room.CreateResponseDTO;
+import com.theo.quixx.service.RoomService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GameController {
 
+    private final RoomService roomService;
+
+    public GameController(RoomService roomService) {
+        this.roomService = roomService;
+    }
+
     @GetMapping("/api/hc")
     public ResponseEntity<?> healthcheck() {
         return ResponseEntity.ok("Server is alive !");
+    }
+
+    @PostMapping("/api/room/create")
+    public CreateResponseDTO createRoom(CreateRequestDTO createRequestDTO) {
+        return new CreateResponseDTO(roomService.createRoom(createRequestDTO).getCode());
     }
 
 }

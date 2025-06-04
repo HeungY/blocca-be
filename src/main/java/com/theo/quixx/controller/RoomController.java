6add57b2ba9be6f0b5b32ebd0437d65d68daf6ee
HeungY <rkrkrkrk02@naver.com>
2025-06-04 17:48:24 +0900

    feat: RoomController RequestBody 추가

--- src/main/java/com/theo/quixx/controller/RoomController.java
+++ src/main/java/com/theo/quixx/controller/RoomController.java
@@ -5,21 +5,20 @@ import com.theo.quixx.dto.room.CreateResponseDTO;
 import com.theo.quixx.dto.room.JoinRequestDTO;
 import com.theo.quixx.dto.room.JoinResponseDTO;
 import com.theo.quixx.service.RoomService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api")
-public class GameController {
+public class RoomController {
 
     private final RoomService roomService;
-
-    public GameController(RoomService roomService) {
-        this.roomService = roomService;
-    }
 
     @GetMapping("/hc")
     public ResponseEntity<?> healthcheck() {
@@ -27,12 +26,12 @@ public class GameController {
     }
 
     @PostMapping("/room/create")
-    public CreateResponseDTO createRoom(CreateRequestDTO createRequestDTO) {
+    public CreateResponseDTO createRoom(@RequestBody CreateRequestDTO createRequestDTO) {
         return roomService.createRoom(createRequestDTO);
     }
 
     @PostMapping("/room/join")
-    public JoinResponseDTO joinRoom(JoinRequestDTO joinRequestDTO) {
+    public JoinResponseDTO joinRoom(@RequestBody JoinRequestDTO joinRequestDTO) {
         return roomService.joinRoom(joinRequestDTO);
     }
 

fab269187846ae59f486bffe9b1a4a4fe3c0bc2f
HeungY <rkrkrkrk02@naver.com>
2025-06-04 17:48:37 +0900

    feat: WebSocket 설정

--- src/main/java/com/theo/quixx/config/WebSocketConfig.java
+++ src/main/java/com/theo/quixx/config/WebSocketConfig.java
@@ -18,6 +18,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws/game").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/ws/game").setAllowedOriginPatterns("*").withSockJS();
     }
 }

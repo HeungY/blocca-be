9196bc01f9adb9a8bcaa4939b8d5c57501223f03
HeungY <rkrkrkrk02@naver.com>
2025-06-04 17:47:45 +0900

    feat: Player 도메인 내부 잠긴 컬러 확인 기능 구현

--- src/main/java/com/theo/quixx/domain/Player.java
+++ src/main/java/com/theo/quixx/domain/Player.java
@@ -2,8 +2,10 @@ package com.theo.quixx.domain;
 
 import java.util.ArrayList;
 import java.util.EnumMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import lombok.Getter;
 
 @Getter
@@ -11,6 +13,7 @@ public class Player {
     private final String id;
     private final Map<Color, List<Integer>> board;
     private int failCount = 0;
+    private Set<Color> lockedColors;
 
     public Player(String id) {
         this.id = id;
@@ -18,10 +21,13 @@ public class Player {
         for (Color color : Color.values()) {
             board.put(color, new ArrayList<>());
         }
+        lockedColors = new HashSet<>();
     }
 
     public boolean canMark(Color color, int number) {
         if (isLocked(color)) return false;
+
+        if (number == color.getFinalNumber() && !judgeMarkFinalNumber(color, number)) return false;
 
         List<Integer> line = board.get(color);
         if (line.isEmpty()) return true;
@@ -30,8 +36,27 @@ public class Player {
         return color.isAscending() ? number > last : number < last;
     }
 
+    private boolean judgeMarkFinalNumber(Color color, int number){
+        if (isLocked(color)) return false;
+        if (number != color.getFinalNumber()) return false;
+
+        List<Integer> line = board.get(color);
+        if (line.isEmpty()) return false;
+
+        return line.size() >= 5;
+    }
+
+    public void lockColor() {
+        for (Color color : Color.values()) {
+            List<Integer> line = board.get(color);
+            if (line.contains(color.getFinalNumber())) {
+                lockedColors.add(color);
+            }
+        }
+    }
+
     public boolean isLocked(Color color) {
-        return board.get(color).contains(color.getFinalNumber());
+        return lockedColors.contains(color);
     }
 
     public String mark(Color color, int number) {

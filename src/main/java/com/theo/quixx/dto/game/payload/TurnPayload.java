b8098b9c7fdaafb55c3c393b63d6ab90d911ebf9
HeungY <rkrkrkrk02@naver.com>
2025-06-25 14:12:25 +0900

    chore: 상속 관계 변경

--- src/main/java/com/theo/quixx/dto/game/payload/TurnPayload.java
+++ src/main/java/com/theo/quixx/dto/game/payload/TurnPayload.java
@@ -1,12 +1,17 @@
 package com.theo.quixx.dto.game.payload;
 
 import com.theo.quixx.domain.enums.Color;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-public class TurnPayload extends MarkPayload{
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TurnPayload implements GamePayload{
+    private boolean success;
+    private String phase;
+    private Color color;
+    private int number;
     private String turnPlayer;
-
-    public TurnPayload(boolean success, String phase, Color color, int number, String turnPlayer) {
-        super(success, phase, color, number);
-        this.turnPlayer = turnPlayer;
-    }
 }

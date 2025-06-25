f1226f97bd727e0689a4f422d6c62e6a9c2e8918
HeungY <rkrkrkrk02@naver.com>
2025-06-25 14:17:17 +0900

    chore: MarkPayload 내부 failCount, lockedColors 추가.
    프론트와 연결하며 테스트 해보던 중 failCount, lockedColors 정보의 필요성으로 인해 추가.

--- src/main/java/com/theo/quixx/dto/game/payload/MarkPayload.java
+++ src/main/java/com/theo/quixx/dto/game/payload/MarkPayload.java
@@ -1,15 +1,19 @@
 package com.theo.quixx.dto.game.payload;
 
 import com.theo.quixx.domain.enums.Color;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class MarkPayload implements GamePayload {
     private boolean success;
     private String phase;
     private Color color;
     private int number;
-
+    private int failCount;
+    private List<Color> lockedColors;
 }

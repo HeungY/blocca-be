dccce942257b70c618672a4dda36f32a97ce511c
HeungY <rkrkrkrk02@naver.com>
2025-06-25 14:12:55 +0900

    chore: 방 생성시 메세지 보내도록 수정

--- src/main/java/com/theo/quixx/service/RoomService.java
+++ src/main/java/com/theo/quixx/service/RoomService.java
@@ -3,24 +3,28 @@ package com.theo.quixx.service;
 import static com.theo.quixx.util.Constant.GENERATE_CODE_CHARACTERS;
 import static com.theo.quixx.util.Constant.MAX_CODE_LENGTH;
 
-import com.theo.quixx.entity.Room;
+import com.theo.quixx.domain.enums.Action;
+import com.theo.quixx.dto.game.ResponseMessage;
+import com.theo.quixx.dto.game.payload.StartPayload;
 import com.theo.quixx.dto.room.CreateRequestDTO;
 import com.theo.quixx.dto.room.CreateResponseDTO;
 import com.theo.quixx.dto.room.JoinRequestDTO;
 import com.theo.quixx.dto.room.JoinResponseDTO;
+import com.theo.quixx.entity.Room;
 import com.theo.quixx.repository.RoomRepository;
 import java.security.SecureRandom;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class RoomService {
-
+    private final SimpMessagingTemplate messagingTemplate;
     private final RoomRepository roomRepository;
     private final SecureRandom random = new SecureRandom();
+    private final GameService gameService;
 
-    public RoomService(RoomRepository roomRepository) {
-        this.roomRepository = roomRepository;
-    }
 
     public CreateResponseDTO createRoom(CreateRequestDTO createRequestDTO) {    // 방 생성 후 코드 반환
         String roomCode = roomRepository.save(new Room(createRequestDTO.getUsername(), generateUniqueRoomCode())).getCode();
@@ -36,7 +40,25 @@ public class RoomService {
         }
 
         room.join(joinRequestDTO.getUsername());
+        gameService.createGame(room.getCode(),room.getFirstPlayer(),room.getSecondPlayer());
+        roomRepository.save(room);
+
         return new JoinResponseDTO(room.getCode());
+    }
+
+    public void initRoom(String code) {
+        Room room = roomRepository.findByCode(code).orElseThrow();
+
+        messagingTemplate.convertAndSend("/topic/room/"+code,
+                ResponseMessage.builder()
+                        .code(code)
+                        .id("SYSTEM")
+                        .action(Action.GAME_INIT)
+                        .payload(new StartPayload(
+                                room.getFirstPlayer(),
+                                room.getSecondPlayer()
+                        ))
+                        .build());
     }
 
 
